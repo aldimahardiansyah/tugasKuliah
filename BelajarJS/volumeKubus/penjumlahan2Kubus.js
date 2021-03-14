c69470492cbbda6e644bb66b3ed23b4f27bd714b
Aldi Mahardiansyah <aldimdiansyah@gmail.com>
2021-03-14 20:33:16 +0700

    membuat fungsi menjumlahkan 2 volume kubus dengan js

---
+++ BelajarJS/volumeKubus/penjumlahan2Kubus.js
@@ -0,0 +1,9 @@
+var kubus1 = prompt('Masukkan panjang sisi kubus 1: ');
+var kubus2 = prompt('Masukkan panjang sisi kubus 2: ');
+
+var volume1 = kubus1 * kubus1 * kubus1;
+var volume2 = kubus2 * kubus2 * kubus2;
+
+var hasil = volume1 + volume2;
+
+alert('Volume dari kubus 1: ' + volume1 + '. Volume kubus 2: ' + volume2 + '\nJumlah: ' + hasil);
