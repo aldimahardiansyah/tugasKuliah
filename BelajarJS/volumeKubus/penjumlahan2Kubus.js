c5e7ff2ee9b3bbac768b33c9f7e4da7b194c26f2
Aldi Mahardiansyah <aldimdiansyah@gmail.com>
2021-03-14 20:59:44 +0700

    menambahkan function

--- BelajarJS/volumeKubus/penjumlahan2Kubus.js
+++ BelajarJS/volumeKubus/penjumlahan2Kubus.js
@@ -1,9 +1,16 @@
-var kubus1 = prompt('Masukkan panjang sisi kubus 1: ');
-var kubus2 = prompt('Masukkan panjang sisi kubus 2: ');
 
-var volume1 = kubus1 * kubus1 * kubus1;
-var volume2 = kubus2 * kubus2 * kubus2;
+function jumlahVolumeDuaKubus(kubus1, kubus2) {
+    var volume1 = kubus1 * kubus1 * kubus1;
+    var volume2 = kubus2 * kubus2 * kubus2;
 
-var hasil = volume1 + volume2;
+    var hasil = volume1 + volume2;
 
-alert('Volume dari kubus 1: ' + volume1 + '. Volume kubus 2: ' + volume2 + '\nJumlah: ' + hasil);
+    console.log('Volume dari kubus 1: ' + volume1 + '. Volume kubus 2: ' + volume2 + '\nTotal: ' + hasil);
+    return hasil;
+}
+var a = prompt('Masukkan panjang sisi kubus 1: ');
+var b = prompt('Masukkan panjang sisi kubus 2: ');
+
+var result = jumlahVolumeDuaKubus(a, b);
+
+alert('Hasil penjumlahan dari dua volume kubus: ' + result);
