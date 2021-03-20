37e5d21a4fcd55c7fb5626df48fc262e64e6cda1
Aldi Mahardiansyah <41710133+aldimahardiansyah@users.noreply.github.com>
2021-03-20 17:33:42 +0700

    belajar method pada array js

---
+++ BelajarJS/array/method_array/method.js
@@ -0,0 +1,23 @@
+// mendeklarasikan array kosong
+let arr = [];
+
+// menambahkan elemen array
+arr.push('Aldi', true, 18);
+
+// menghapus elemen terakhir array
+arr.pop();
+
+// mengubah array kedalam string dgn separator '-'
+let cetak = arr.join(' - ')
+
+// mencetak
+console.log(cetak)
+
+// menambahkan elemen pertama array
+arr.unshift('Mahar')
+
+// mengubah array kedalam string dgn separator '-'
+let cetak2 = arr.join(' - ')
+
+// mencetak
+console.log(cetak2)
