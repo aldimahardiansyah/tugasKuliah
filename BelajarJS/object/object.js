08c9cccd78ee1450469305bb14ac5053d285226e
Aldi Mahardiansyah <aldimdiansyah@gmail.com>
2021-03-31 21:24:26 +0700

    belajar object js

---
+++ BelajarJS/object/object.js
@@ -0,0 +1,10 @@
+let mhs = {
+    "nama": "Aldi Mahardiansyah",
+    "jurusan": "Teknik Informatika",
+    "ips": [3.97, 3.92, 3.76],
+    "alamat": {
+        "jalan": "Jl. Abc No. 123",
+        "kota": "Bogor",
+        "Provinsi": "Jawa Barat"
+    }
+};
