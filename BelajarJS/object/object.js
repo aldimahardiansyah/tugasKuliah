99c127c2bed074e4b40d539492e73013381daa2d
Aldi Mahardiansyah <aldimdiansyah@gmail.com>
2021-04-01 20:40:05 +0700

    belajar membuat objek js

--- BelajarJS/object/object.js
+++ BelajarJS/object/object.js
@@ -1,3 +1,4 @@
+// objek literal
 let mhs = {
     "nama": "Aldi Mahardiansyah",
     "jurusan": "Teknik Informatika",
@@ -8,3 +9,31 @@ let mhs = {
         "Provinsi": "Jawa Barat"
     }
 };
+
+// objek function declaration
+let objekMahasiswa = function (nama, jurusan, nim, kota) {
+    let mahasiswa = {};
+    mahasiswa.nama = nama;
+    mahasiswa.jurusan = jurusan;
+    mahasiswa.nim = nim;
+    mahasiswa.kota = kota;
+    return mahasiswa;
+}
+
+let mhs1 = objekMahasiswa('Dody', 'Sistem Informasi', 0110220223, 'Depok');
+let mhs2 = objekMahasiswa('Rendi', 'Sistem Informasi', 0110220243, 'Tegal');
+
+
+// objek constructor
+class Mahasiswa {
+    constructor(nama, jurusan, nim, kota) {
+        this.nama = nama;
+        this.jurusan = jurusan;
+        this.nim = nim;
+        this.kota = kota;
+    }
+}
+
+let mhs3 = new Mahasiswa('Adi', 'Teknik Pangan', 011022736, 'Jakarta');
+let mhs4 = new Mahasiswa('Roy', 'Kedokteran', 0110220376, 'Tanggerang');
+
