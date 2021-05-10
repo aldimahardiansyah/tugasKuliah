c858922d01d9de0df5da9d222b56d5b3871d84ec
Aldi Mahardiansyah <aldimdiansyah@gmail.com>
2021-05-10 15:57:45 +0700

    final study in js dicoding

---
+++ BelajarJS/dicoding/node/latihantesting/index.js
@@ -0,0 +1,10 @@
+/* 
+test(‘deskripsi dari testcase kamu’, () => {
+    expect(perintah).matcher(nilai yang diekspektasikan);
+}); 
+*/
+
+// contoh
+test('dua tambah dua adalah empat', () => {
+    expect(2 + 2).toBe(4);
+});
